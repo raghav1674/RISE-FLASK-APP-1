868654841f85a74298dba9decf4858216f65fb2e
raghav1674 <raghav.81-cse-17@mietjammu.in>
2021-04-13 01:09:26 +0530

    added: Per User Post functionality and used redis db for storing session details

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 from os import environ
 
-
+from functools import wraps
 from redis_util import get_connection, set_value, get_value
-from models import db, Post
+from models import db, Post, User
 
 
 # loading the environtmental variables from .env file
@@ -27,17 +27,73 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 db.create_all(app=app)
 
+
+def login_required(fn):
+    @wraps(fn)
+    def inner_func(*args, **kwargs):
+        if get_value(con, "logged_in")[1]:
+            return fn(*args, **kwargs)
+        else:
+            return {"message": "Login Required"}
+    return inner_func
+
+
+@app.route("/user/register", methods=["POST"])
+def register_user():
+    if request.method == "POST":
+        email = request.form.get("email", None)
+        password = request.form.get("password", None)
+
+        if email and password:
+            old_user = User.query.filter_by(email=email).first()
+            if not old_user:
+                new_user = User(email=email, password=password)
+                db.session.add(new_user)
+                db.session.commit()
+                return {"message": "Account Created Successfully"}
+            else:
+                return {"message": "User Already present"}
+        return {"message": "Please provide the email and password"}
+    return {"message": "Method Not Allowed"}
+
+
+@app.route("/user/login", methods=["POST"])
+def login_user():
+    if request.method == "POST":
+        email = request.form.get("email", None)
+        password = request.form.get("password", None)
+
+        if email and password:
+            user = User.query.filter_by(email=email).first()
+            if user and user.get_password(password=password):
+                set_value(con, "email", email)
+                set_value(con,"user_id",user.user_id)
+                set_value(con, "logged_in", True)
+                return {"message": f"{email} have been logged In Successfully"}
+            else:
+                return {"message": "Invalid Credentials"}
+        return {"message": "Please provide email and password"}
+    return {"message": "Method Not Allowed"}
+
+
+@app.route("/user/logout")
+@login_required
+def logout_user():
+    set_value(con, "logged_in", False)
+    set_value(con, "email", None)
+    return {"message": "Logged Out Successfully"}
+
+
 # get posts
-
-
 @app.route("/posts")
 @app.route("/posts/<int:id>", methods=["GET"])
+@login_required
 def get_post(id=None):
     if id is None:
-        posts = Post.query.all()
+        posts = Post.query.filter_by(user_id=get_value(con,"user_id")[1])
         return jsonify({'posts': [post.serialize() for post in posts]})
     else:
-        post = Post.query.filter_by(post_id=id).first()
+        post = Post.query.filter_by(post_id=id,user_id=get_value(con,"user_id")[1]).first()
         if post:
             return post.serialize()
     return {"error": "Please provide correct id"}
@@ -46,25 +102,26 @@ def get_post(id=None):
 
 
 @app.route("/posts", methods=["POST"])
+@login_required
 def create_post():
     title = request.form.get("title", None)
     if title is not None:
-        post = Post(title=title)
+        post = Post(title=title,user_id=get_value(con,"user_id")[1])
         db.session.add(post)
         db.session.commit()
         return jsonify(post.serialize())
-
     return {"error": "Please provide title"}
 
 # update post
 
 
 @app.route("/posts/<int:id>", methods=["PUT"])
+@login_required
 def update_post(id):
     title = request.form.get("title", None)
 
     if title is not None and id is not None:
-        oldpost = Post.query.filter_by(post_id=id).first()
+        oldpost = Post.query.filter_by(post_id=id,user_id=get_value(con,"user_id")[1]).first()
         if oldpost:
             oldpost.title = title
             db.session.add(oldpost)
@@ -78,9 +135,10 @@ def update_post(id):
 
 # delete post
 @app.route("/posts/<int:id>", methods=["DELETE"])
+@login_required
 def delete_post(id):
     if id is not None:
-        deletedpost = Post.query.filter_by(post_id=id).first()
+        deletedpost = Post.query.filter_by(post_id=id,user_id=get_value(con,"user_id")[1]).first()
         db.session.delete(deletedpost)
         db.session.commit()
         return jsonify(deletedpost.serialize())
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-
-# database 
+from werkzeug.security import generate_password_hash, check_password_hash
+# database
 db = SQLAlchemy()
 
 
@@ -9,6 +9,43 @@ class Post(db.Model):
 
     post_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'user.user_id'), nullable=False)
+
+    def __init__(self, title,user_id):
+        self.title = title
+        self.user_id = user_id 
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def __repr__(self):
+        return f'{self.title}'
 
     def serialize(self):
-        return {"_id":self.post_id,"title":self.title}
+        return {"_id": self.post_id, "title": self.title}
+
+
+class User(db.Model):
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    # posts = db.relationship('Post', backref='user', lazy=True)
+    
+    def __init__(self, email, password):
+
+        self.email = email
+        self.password = self.set_password(password)
+
+    def set_password(self, password):
+        return generate_password_hash(password)
+
+    def get_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __str__(self):
+        return f'{self.email}'
+
+    def __repr__(self):
+        return f'{self.email}'
